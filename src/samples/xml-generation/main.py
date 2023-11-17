36d6d5c607079106fee27aa1cd8be843a69b6514
Tafonso91 <t.veloso.afonso@ipvc.pt>
2023-11-17 23:08:17 +0000

    adicionei o schema

--- src/samples/xml-generation/main.py
+++ src/samples/xml-generation/main.py
@@ -2,4 +2,14 @@ from csv_to_xml_converter import CSVtoXMLConverter
 
 if __name__ == "__main__":
     converter = CSVtoXMLConverter("/data/fifa_23.csv")
-    print(converter.to_xml_str())
+    xml_str = converter.to_xml_str()  # Obtém a representação XML como string
+
+    # Escreve a string XML em um arquivo
+    with open("/data/fifa_23.xml", "w") as xml_file:
+        xml_file.write(xml_str)
+
+    print("Arquivo XML 'fifa_23.xml' criado com sucesso!")
+
+
+
+    
